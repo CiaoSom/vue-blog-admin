eb8a01b993ca3c107fc7019572af04f471692572
calvinCar <m17610602708@163.com>
2018-07-21 13:08:47 +0800

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,19 @@
+import category from '@/api/category'
+import content from '@/api/content'
+import comment from '@/api/comment'
+import image from '@/api/image'
+import tag from '@/api/tag'
+import token from '@/api/token'
+import user from '@/api/user'
+import config from '@/api/config'
+
+export {
+  category,
+  content,
+  comment,
+  image,
+  tag,
+  token,
+  user,
+  config
+}
